b390e1d7b2b5217dd94b57e4b3c46631edc35594
jospurs <johanvialard31@gmail.com>
2026-05-10 17:32:13 +0200

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://abvnmxtzyiletjjymfhj.supabase.co/rest/v1/'   // ← colle ta Project URL
+const SUPABASE_URL = 'https://abvnmxtzyiletjjymfhj.supabase.co/rest/v1'   // ← colle ta Project URL
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFidm5teHR6eWlsZXRqanltZmhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyNjc0MTksImV4cCI6MjA5Mzg0MzQxOX0.MvXmTDvI9gJICwVTbmZrnsirGFT96iwpe32KbckXou4'                 // ← colle ta anon key
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
